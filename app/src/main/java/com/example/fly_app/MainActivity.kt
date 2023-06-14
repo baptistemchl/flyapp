743e99303044a9b6a8f59600c61ced9b5bbfb560
Baptiste <baptistemichel22@gmail.com>
2023-06-14 19:52:27 +0200

    Styling cards for AirportScreen

--- app/src/main/java/com/example/fly_app/MainActivity.kt
+++ app/src/main/java/com/example/fly_app/MainActivity.kt
@@ -1,68 +1,66 @@
 package com.example.fly_app
 
 import AirportScreen
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.fly_app.screens.flight.FlightScreen
-import com.example.fly_app.screens.flight.FlightViewModel
 import com.example.fly_app.screens.fly.FlyScreen
 import com.example.fly_app.screens.home.HomeScreen
-import com.example.fly_app.data.service.ApiService
 import com.example.fly_app.ui.BottomNavItem
 import com.example.fly_app.ui.theme.Fly_appTheme
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.LatLng
-import kotlinx.coroutines.launch
-import locationFlow
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
-    private val fusedLocationClient: FusedLocationProviderClient by lazy {
-        LocationServices.getFusedLocationProviderClient(this)
-    }
-    private val flightViewModel: FlightViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Fly_appTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreenView()
-                }
+                MainScreen()
             }
         }
     }
 }
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationComponent(navController = navController) }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFFF5F5DC) // Utilise la couleur beige comme fond
+        ) {
+            NavigationGraph(navController = navController)
+        }
+    }
+}
+
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
@@ -83,7 +81,7 @@ fun NavigationGraph(navController: NavHostController) {
 }
 
 @Composable
-fun BottomNavigationComposant(navController: NavController) {
+fun BottomNavigationComponent(navController: NavHostController) {
     val items = listOf(
         BottomNavItem.Home,
         BottomNavItem.Fly,
@@ -91,31 +89,43 @@ fun BottomNavigationComposant(navController: NavController) {
         BottomNavItem.Airport,
     )
     BottomNavigation(
-        backgroundColor = colorResource(id = R.color.teal_200),
-        contentColor = colorResource(id = R.color.black)
+        backgroundColor = colorResource(id = R.color.bordeaux),
+        contentColor = colorResource(id = R.color.black),
+        elevation = 10.dp // Supprime l'ombre en définissant l'élévation à 0dp
     ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
-                label = {
-                    Text(
-                        text = item.title,
-                        fontSize = 9.sp
+                icon = {
+                    Icon(
+                        painterResource(id = item.icon),
+                        contentDescription = item.title,
+                        tint = if (currentRoute == item.screen_route) {
+                            LocalContentColor.current
+                        } else {
+                            LocalContentColor.current.copy(alpha = 0.4f)
+                        }
                     )
                 },
-                selectedContentColor = colorResource(id = R.color.black),
-                unselectedContentColor = colorResource(id = R.color.black).copy(0.4f),
-                alwaysShowLabel = true,
+                label = {
+                    val textColor = if (currentRoute == item.screen_route) {
+                        Color(0xFFF5F5DC) /// Couleur grise pour le label sélectionné
+                    } else {
+                        Color(0xFF808080) //
+                        // Couleur beige pour le label non sélectionné
+                    }
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp,
+                        color = textColor
+                    )
+                },
                 selected = currentRoute == item.screen_route,
                 onClick = {
                     navController.navigate(item.screen_route) {
-
-                        navController.graph.startDestinationRoute?.let { screen_route ->
-                            popUpTo(screen_route) {
-                                saveState = true
-                            }
+                        popUpTo(navController.graph.startDestinationRoute!!) {
+                            saveState = true
                         }
                         launchSingleTop = true
                         restoreState = true
@@ -126,12 +136,4 @@ fun BottomNavigationComposant(navController: NavController) {
     }
 }
 
-@Composable
-fun MainScreenView() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigationComposant(navController = navController) }
-    ) {
-        NavigationGraph(navController = navController)
-    }
-}
+
--- app/src/main/java/com/example/fly_app/screens/airport/AirportScreen.kt
+++ app/src/main/java/com/example/fly_app/screens/airport/AirportScreen.kt
@@ -1,13 +1,23 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,9 +27,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.fly_app.R
 import com.example.fly_app.screens.airport.AirportViewModel
 
 @Composable
@@ -36,6 +57,7 @@ fun AirportScreen(airportViewModel: AirportViewModel = viewModel()) {
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = 56.dp) // Réserve l'espace pour la BottomNavigationBar
+
     ) {
         airportDataList?.let { data ->
             if (data.isNotEmpty()) {
@@ -43,23 +65,92 @@ fun AirportScreen(airportViewModel: AirportViewModel = viewModel()) {
                     modifier = Modifier
                         .verticalScroll(rememberScrollState())
                         .padding(horizontal = 16.dp)
+                        .padding(top = 40.dp) // Ajoute une marge supérieure pour éviter le chevauchement
                 ) {
-                    Text("Airport Data:")
                     data.subList(0, visibleAirportCount.value).forEach { airport ->
-                        Card(
+                        Column(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(vertical = 8.dp)
+                                .padding(bottom = 16.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(color = Color.White)
+                                .clickable { /* Action on airport card click */ }
+                                .padding(16.dp)
                         ) {
-                            Column(
-                                modifier = Modifier.padding(16.dp)
-                            ) {
-                                Text("Name: ${airport.name}")
-                                Text("IATA Code: ${airport.iataCode}")
-                                Text("ICAO Code: ${airport.icaoCode}")
-                                Text("Latitude: ${airport.lat}")
-                                Text("Longitude: ${airport.lng}")
+                            airport.name?.let {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_airport),
+                                        contentDescription = "Airport Icon",
+                                        tint = Color(0xFF888888) // Couleur de l'icône
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(
+                                        text = "${airport.name}",
+                                        style = TextStyle(
+                                            fontSize = 16.sp,
+                                            fontWeight = FontWeight.Bold,
+                                            color = Color(0xFF333333) // Couleur du texte du nom de l'aéroport
+                                        ),
+                                        modifier = Modifier.weight(1f) // Permet de centrer le texte avec l'icône
+                                    )
+                                }
                             }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = buildAnnotatedString {
+                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                        append("IATA Code:")
+                                    }
+                                    append(" ${airport.iataCode}")
+                                },
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    color = Color(0xFF666666) // Couleur du texte du code IATA
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = buildAnnotatedString {
+                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                        append("ICAO Code:")
+                                    }
+                                    append(" ${airport.icaoCode}")
+                                },
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    color = Color(0xFF666666) // Couleur du texte du code ICAO
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = buildAnnotatedString {
+                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                        append("Latitude:")
+                                    }
+                                    append(" ${airport.lat}")
+                                },
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    color = Color(0xFF666666) // Couleur du texte de la latitude
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = buildAnnotatedString {
+                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                        append("Longitude:")
+                                    }
+                                    append(" ${airport.lng}")
+                                },
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    color = Color(0xFF666666) // Couleur du texte de la longitude
+                                )
+                            )
                         }
                     }
                     if (visibleAirportCount.value < data.size) {
@@ -80,6 +171,27 @@ fun AirportScreen(airportViewModel: AirportViewModel = viewModel()) {
                     SpinningIndicator()
                 }
             }
+        }
+        Surface(
+            modifier = Modifier
+
+                .fillMaxWidth()
+                .align(Alignment.TopCenter),
+            color = Color(0xFFFFF3E0), // Couleur de fond beige
+            elevation = 4.dp, // Niveau d'élévation pour l'ombre
+
+        ) {
+            Text(
+                text = "Airport Data",
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF333333) // Couleur du texte de l'en-tête
+                )
+            )
         }
     }
 }
