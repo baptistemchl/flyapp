42796573f3aba6b7bca1f089f0d8e87329a67f24
Baptiste <baptistemichel22@gmail.com>
2023-06-20 16:16:13 +0200

    Schedule api

--- app/src/main/java/com/example/fly_app/screens/airport/AirportScreen.kt
+++ app/src/main/java/com/example/fly_app/screens/airport/AirportScreen.kt
@@ -15,11 +15,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,9 +45,20 @@ import androidx.navigation.NavController
 import com.example.fly_app.R
 import com.example.fly_app.screens.airport.AirportViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AirportScreen(airportViewModel: AirportViewModel = viewModel(), navController: NavController,) {
+fun AirportScreen(airportViewModel: AirportViewModel = viewModel(), navController: NavController) {
     val airportDataList by airportViewModel.airportDataList.collectAsState()
+    val searchQuery = remember { mutableStateOf("") }
+    val filteredAirportDataList = remember {
+        derivedStateOf {
+            airportDataList?.filter { airport ->
+                airport.name?.contains(searchQuery.value, ignoreCase = true) ?: false ||
+                        airport.iataCode?.contains(searchQuery.value, ignoreCase = true) ?: false ||
+                        airport.icaoCode?.contains(searchQuery.value, ignoreCase = true) ?: false
+            }
+        }
+    }
 
     LaunchedEffect(key1 = airportViewModel) {
         airportViewModel.fetchAirportData()
@@ -55,147 +70,165 @@ fun AirportScreen(airportViewModel: AirportViewModel = viewModel(), navControlle
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = 56.dp) // Réserve l'espace pour la BottomNavigationBar
-
     ) {
-        airportDataList?.let { data ->
-            if (data.isNotEmpty()) {
-                Column(
+        Column(modifier = Modifier.fillMaxSize()) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color(0xFFFFF3E0), // Couleur de fond beige
+                elevation = 4.dp, // Niveau d'élévation pour l'ombre
+            ) {
+                Text(
+                    text = "Airport Data",
                     modifier = Modifier
-                        .verticalScroll(rememberScrollState())
-                        .padding(horizontal = 16.dp)
-                        .padding(top = 40.dp) // Ajoute une marge supérieure pour éviter le chevauchement
-                ) {
-                    data.subList(0, visibleAirportCount.value).forEach { airport ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(bottom = 16.dp)
-                                .clip(RoundedCornerShape(8.dp))
-                                .background(color = Color.White)
-                                .clickable {
-                                    navController.navigate("schedule/${airport.iataCode}")
-                                }
-                                .padding(16.dp)
-                        ) {
-                            airport.name?.let {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    modifier = Modifier.fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF333333) // Couleur du texte de l'en-tête
+                    )
+                )
+            }
+            TextField(
+                value = searchQuery.value,
+                onValueChange = { newValue ->
+                    searchQuery.value = newValue
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .background(Color.White), // Définir la couleur de fond du TextField
+                placeholder = { Text("Search airports") },
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                singleLine = true
+            )
+
+            airportDataList?.let { data ->
+                if (filteredAirportDataList.value?.isNotEmpty() == true) {
+                    Column(
+                        modifier = Modifier
+                            .verticalScroll(rememberScrollState())
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 8.dp) // Ajoute une marge supérieure pour éviter le chevauchement
+                    ) {
+                        filteredAirportDataList.value?.subList(0, visibleAirportCount.value)
+                            ?.forEach { airport ->
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(bottom = 16.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                        .background(color = Color.White)
+                                        .clickable {
+                                            navController.navigate("schedule/${airport.iataCode}")
+                                        }
+                                        .padding(16.dp)
                                 ) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_airport),
-                                        contentDescription = "Airport Icon",
-                                        tint = Color(0xFF888888) // Couleur de l'icône
-                                    )
-                                    Spacer(modifier = Modifier.width(8.dp))
+                                    airport.name?.let {
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.fillMaxWidth()
+                                        ) {
+                                            Icon(
+                                                painter = painterResource(id = R.drawable.ic_airport),
+                                                contentDescription = "Airport Icon",
+                                                tint = Color(0xFF888888) // Couleur de l'icône
+                                            )
+                                            Spacer(modifier = Modifier.width(8.dp))
+                                            Text(
+                                                text = "${airport.name}",
+                                                style = TextStyle(
+                                                    fontSize = 16.sp,
+                                                    fontWeight = FontWeight.Bold,
+                                                    color = Color(0xFF333333) // Couleur du texte du nom de l'aéroport
+                                                ),
+                                                modifier = Modifier.weight(1f) // Permet de centrer le texte avec l'icône
+                                            )
+                                        }
+                                    }
+                                    Spacer(modifier = Modifier.height(8.dp))
                                     Text(
-                                        text = "${airport.name}",
+                                        text = buildAnnotatedString {
+                                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                                append("IATA Code:")
+                                            }
+                                            append(" ${airport.iataCode}")
+                                        },
                                         style = TextStyle(
-                                            fontSize = 16.sp,
-                                            fontWeight = FontWeight.Bold,
-                                            color = Color(0xFF333333) // Couleur du texte du nom de l'aéroport
-                                        ),
-                                        modifier = Modifier.weight(1f) // Permet de centrer le texte avec l'icône
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF666666) // Couleur du texte du code IATA
+                                        )
+                                    )
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Text(
+                                        text = buildAnnotatedString {
+                                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                                append("ICAO Code:")
+                                            }
+                                            append(" ${airport.icaoCode}")
+                                        },
+                                        style = TextStyle(
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF666666) // Couleur du texte du code ICAO
+                                        )
+                                    )
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Text(
+                                        text = buildAnnotatedString {
+                                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                                append("Latitude:")
+                                            }
+                                            append(" ${airport.lat}")
+                                        },
+                                        style = TextStyle(
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF666666) // Couleur du texte de la latitude
+                                        )
+                                    )
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Text(
+                                        text = buildAnnotatedString {
+                                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                                append("Longitude:")
+                                            }
+                                            append(" ${airport.lng}")
+                                        },
+                                        style = TextStyle(
+                                            fontSize = 14.sp,
+                                            color = Color(0xFF666666) // Couleur du texte de la longitude
+                                        )
                                     )
                                 }
                             }
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Text(
-                                text = buildAnnotatedString {
-                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                        append("IATA Code:")
-                                    }
-                                    append(" ${airport.iataCode}")
+                        if (visibleAirportCount.value < filteredAirportDataList.value!!.size) {
+                            Button(
+                                onClick = {
+                                    visibleAirportCount.value += 5
                                 },
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    color = Color(0xFF666666) // Couleur du texte du code IATA
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = buildAnnotatedString {
-                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                        append("ICAO Code:")
-                                    }
-                                    append(" ${airport.icaoCode}")
-                                },
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    color = Color(0xFF666666) // Couleur du texte du code ICAO
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = buildAnnotatedString {
-                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                        append("Latitude:")
-                                    }
-                                    append(" ${airport.lat}")
-                                },
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    color = Color(0xFF666666) // Couleur du texte de la latitude
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = buildAnnotatedString {
-                                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                                        append("Longitude:")
-                                    }
-                                    append(" ${airport.lng}")
-                                },
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    color = Color(0xFF666666) // Couleur du texte de la longitude
-                                )
-                            )
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 16.dp)
+                            ) {
+                                Text("Voir plus")
+                            }
                         }
                     }
-                    if (visibleAirportCount.value < data.size) {
-                        Button(
-                            onClick = {
-                                visibleAirportCount.value += 5
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 16.dp)
-                        ) {
-                            Text("Voir plus")
-                        }
+                } else {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("No airports found")
                     }
-                }
-            } else {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    SpinningIndicator()
                 }
             }
         }
-        Surface(
-            modifier = Modifier
-
-                .fillMaxWidth()
-                .align(Alignment.TopCenter),
-            color = Color(0xFFFFF3E0), // Couleur de fond beige
-            elevation = 4.dp, // Niveau d'élévation pour l'ombre
-
-        ) {
-            Text(
-                text = "Airport Data",
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                textAlign = TextAlign.Center,
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF333333) // Couleur du texte de l'en-tête
-                )
-            )
-        }
     }
 }
-
 
 @Composable
 fun SpinningIndicator() {
